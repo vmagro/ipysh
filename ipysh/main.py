b8fb1246ff5b0fbd5050ca8cabbaa0ac95a7a43e
Vinnie Magro <v@vinnie.io>
2018-12-01 16:37:18 -0500

    Basic IPython config with custom cwd prompt

--- ipysh/main.py
+++ ipysh/main.py
@@ -1,2 +1,26 @@
+import IPython
+from traitlets.config.loader import Config
+
+from pathlib import Path
+
+from ipysh.prompt import DefaultPrompt
+
+
 def ipysh():
-    print('Welcome')
+    cfg = Config()
+
+    cfg.TerminalInteractiveShell.confirm_exit = False
+
+    cfg.TerminalInteractiveShell.prompts_class = DefaultPrompt
+
+    cfg.TerminalInteractiveShell.ipython_dir = str(Path.home() / '.ipysh')
+    cfg.BaseIPythonApplication.ipython_dir = str(Path.home() / '.ipysh')
+    cfg.BaseIPythonApplication.copy_config_files = True
+    
+    cfg.InteractiveShell.show_rewritten_input = False
+    cfg.InteractiveShell.autocall = 1
+    cfg.InteractiveShellApp.exec_lines = [
+        '%rehashx',
+    ]
+
+    IPython.start_ipython(config=cfg)
---
+++ ipysh/prompt.py
@@ -0,0 +1,40 @@
+from IPython.terminal.prompts import Prompts, Token
+
+import os
+from pathlib import Path
+from getpass import getuser
+from socket import gethostname
+
+
+# build a map of symlinks in ~ that we can "unresolve"
+symlink_map = {}
+home = Path.home()
+for child in home.iterdir():
+    if child.is_symlink():
+        symlink_map[str(child.resolve())] = str(child)
+
+
+class DefaultPrompt(Prompts):
+    def in_prompt_tokens(self, cli=None):
+        cwd = os.getcwd()
+        symlink_matches = [s for s in symlink_map.keys() if cwd.startswith(s)]
+        if symlink_matches:
+            prefix = max(symlink_matches, key=len)
+            cwd = cwd.replace(prefix, symlink_map[prefix])
+        cwd = cwd.replace(str(home), '~')
+        return [
+            (Token, getuser()),
+            (Token, ' at '),
+            (Token, gethostname()),
+            (Token, ' in '),
+            (Token.Keyword, cwd),
+            (Token, '\n'),
+            (Token.Prompt, '$ '),
+        ]
+
+    def out_prompt_tokens(self):
+        return [
+            (Token.OutPrompt, 'Out<'),
+            (Token.OutPromptNum, str(self.shell.execution_count)),
+            (Token.OutPrompt, '>: '),
+        ]
